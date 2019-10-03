3cec1b1025359d1e3ce490892ae00edf172b8288
Lucas Homer <lucashomer@lucass-mbp.home>
2019-10-02 20:55:23 -0700

    second commit to test git functionality

--- src/App.js
+++ src/App.js
@@ -10,14 +10,7 @@ function App() {
         <p>
           Food Oasis
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
