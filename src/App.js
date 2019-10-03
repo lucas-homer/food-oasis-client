019dabf4afbb008c6a3eb8032c39bd705edcd019
Lucas Homer <lucashomer@lucass-mbp.home>
2019-10-02 20:38:09 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Food Oasis
         </p>
         <a
           className="App-link"
