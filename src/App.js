ca2b483cefd7040b5f47844028ed4cb35be5cb5d
Lucas Homer <lucashomer@lucass-mbp.home>
2019-10-02 22:00:10 -0700

    bring in fola logo and adjust some styles for fun

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./images/fola.svg";
+import "./App.css";
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  description: {
+    maxWidth: "35%",
+    fontSize: "1.2rem",
+  },
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Food Oasis
-        </p>
+        <p style={{ fontSize: "2rem" }}>Food Oasis Los Angeles</p>
 
+        <section style={styles.container}>
+          <article style={styles.description}>
+            Food Oasis LALA (FOLA) shows you places to find healthy food in Los
+            Angeles - whether you are looking to buy, grow, or need access to
+            free food.
+          </article>
+        </section>
       </header>
     </div>
   );
